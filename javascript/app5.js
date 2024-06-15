39173cae9878e1da89f97acfdad6f27099b8fd9a
Sebastian <juansebasbeltran19@gmail.com>
2024-06-15 07:04:06 -0500

    segunda vesrion

--- javascript/app5.js
+++ javascript/app5.js
@@ -2,6 +2,7 @@ const $inicial = document.getElementById("inicial");
 const $inicialBrillo = document.getElementById("inicial-brillo");
 const $mariposa = document.getElementById("mariposa");
 const $setCorazones = document.getElementById("set-corazones");
+const $choquerCadenaPlana = document.getElementById("choquer-cadena-plana")
 const $setFiguras = document.getElementById("set-figuras");
 
 
@@ -28,6 +29,11 @@ $setCorazones.addEventListener("click", function(event){
     event.preventDefault();
     document.location.href = "productos/setcorazones.html";
 })
+
+$choquerCadenaPlana.addEventListener("click", function(event){
+    event.preventDefault();
+    document.location.href = "productos/choquercadenaplana.html";
+});
 
 $setFiguras.addEventListener("click", function(event){
     event.preventDefault();
--- javascript/app6.js
+++ javascript/app6.js
@@ -1,13 +1,29 @@
 const $earCuffColores = document.getElementById("ear-cuff-colores");
+const $toposFiguras = document.getElementById("topos-figuras");
 const $earCuffFiguras = document.getElementById("ear-cuff-figuras");
+const $earCuffElegante = document.getElementById("ear-cuff-elegante");
 const $mandala = document.getElementById("mandala");
+const $toposDeMiniColor = document.getElementById("topos-de-mini-color");
+const $toposDeSeguridadFiguras = document.getElementById("topos-de-seguridad-figuras");
 const $topoSeguridadPerla = document.getElementById("topo-seguridad-perla");
+const $mediasGym = document.getElementById("medias-gym");
 
 
 $earCuffColores.addEventListener("click", function(event){
     event.preventDefault();
     document.location.href = "productos/earcuffcolores.html";
 });
+
+$toposFiguras.addEventListener("click", function(event){
+    event.preventDefault();
+    document.location.href = "productos/toposfiguras.html";
+});
+
+$earCuffElegante.addEventListener("click", function(event){
+    event.preventDefault();
+    document.location.href = "productos/earcuffelegante.html";
+});
+
 
 $earCuffFiguras.addEventListener("click", function(event){
     event.preventDefault();
@@ -23,6 +39,21 @@ $mandala.addEventListener("click", function(event){
 $topoSeguridadPerla.addEventListener("click", function(event){
     event.preventDefault();
     document.location.href = "productos/toposeguridadperla.html";
+})
+
+$toposDeMiniColor.addEventListener("click", function(event){
+    event.preventDefault();
+    document.location.href = "productos/toposdeminicolor.html";
+})
+
+$toposDeSeguridadFiguras.addEventListener("click", function(event){
+    event.preventDefault();
+    document.location.href = "productos/toposdeseguridadfiguras.html";
+})
+
+$mediasGym.addEventListener("click", function(event){
+    event.preventDefault();
+    document.location.href = "productos/mediasGYM.html";
 })
 
 
